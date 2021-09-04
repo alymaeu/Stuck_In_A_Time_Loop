d195aedabfe149d496ad47e13f2931dc04fa8b0a
alyma <alyma@MARK-JR>
2021-09-04 15:45:30 -0400

    Add project files.

---
+++ Stuck_In_A_Time_Loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Stuck_In_A_Time_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int N = 1; N <= x; N++)
+            {
+                Console.WriteLine(N +" Abracadabra");
+                    }
+        }
+
+    }
+    }
